6825a0fdb31f7f896906b831e0fde0912529a90a
Justin Torres <justin@movista.com>
2020-05-03 01:34:39 -0500

    Updates

---
+++ precache-manifest.066778fff208dfcb3fdc082d33a8c977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035375c8d2d64fa1481156b8284405ba",
+    "url": "/react_course_monster_rolodex/index.html"
+  },
+  {
+    "revision": "626a5190b5a8cc730a7a",
+    "url": "/react_course_monster_rolodex/static/css/main.5ae2773e.chunk.css"
+  },
+  {
+    "revision": "2d8ff8481de9f1bcc499",
+    "url": "/react_course_monster_rolodex/static/js/2.0ad58468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_course_monster_rolodex/static/js/2.0ad58468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626a5190b5a8cc730a7a",
+    "url": "/react_course_monster_rolodex/static/js/main.474fd450.chunk.js"
+  },
+  {
+    "revision": "b1226d03dba45dc93435",
+    "url": "/react_course_monster_rolodex/static/js/runtime-main.87462ec4.js"
+  }
+]);
